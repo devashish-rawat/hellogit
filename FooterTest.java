475f3e148853ddc5eadf424f00a3aa012e74dc94
Devashish Rawat <dev.rawat7396@gmail.com>
2020-03-16 01:35:45 +0530

    supp

---
+++ FooterTest.java
@@ -0,0 +1,14 @@
+package testpackage;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Devashish on 09-12-2019.
+ */
+public class FooterTest {
+
+    @Test
+    public void socialMediaIcons(){
+        System.out.println("Social Media Icons");
+    }
+}
